69a3dd14960841b03206a366b1b7c3a048ef7b8a
liyuchao <414030511@qq.com>
2018-09-04 16:18:22 +0800

    第一次提交

---
+++ test.js
@@ -0,0 +1,17 @@
+// var ary = [1,2,3,4];
+// function fn(ary) {
+//     ary[0] = 0;
+//     ary = [0];
+//     ary[0] = 100;
+//     return ary;
+// }
+// var res =fn(ary);
+// console.log(res);
+
+function getChildren(ele) {
+    var ary = [];
+    var ys = ele.childNodes;
+}
+var oUl = document.getElementById("list");
+getChildren(oUl);
+
